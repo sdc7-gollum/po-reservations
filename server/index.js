8557333ab896e4f7fb8e97c7017fd3c36894126a
Benjamin Barnett <ben.barnett@gmail.com>
2020-05-01 22:55:30 -0700

    Add Mongoose model query to GET request handler
    The API now responds with the document with the matching ID. /api/room/1
    returns record 1, and so on.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-console */
+
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
-const { db } = require('./database.js');
+const { Room } = require('./database.js');
 
 const app = express();
 
@@ -10,11 +12,18 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 
-app.route('/api/room/:id')
+app.route('/api/room/:id') // /:id
   .get((req, res) => {
     // database retrieval goes here
     // placeholder:
-    res.sendStatus(200);
+    console.log('ID:', req.params.id);
+    Room.find({ id: req.params.id }, (err, record) => {
+      if (err) {
+        console.log('Retrieval error:', err);
+        res.sendStatus(500);
+      }
+      res.status(200).send(record);
+    });
   })
   .post((req, res) => {
     // database insertion goes here
