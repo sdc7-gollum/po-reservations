7cdd1271565383ad10e3b29dc8ae97fe32e5a112
Benjamin Barnett <ben.barnett@gmail.com>
2020-05-09 01:12:28 -0700

    Factor out the top row into its own component

--- client/Reservation.jsx
+++ client/Reservation.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Costs from './Components/Costs.jsx';
 import Button from './Components/Button.jsx';
 import Form from './Components/Form.jsx';
+import TopRow from './Components/TopRow.jsx';
 import styles from './Reservation.css';
 
 class Reservation extends Component {
@@ -38,29 +39,10 @@ class Reservation extends Component {
   }
 
   render() {
-    const { price, reviewScore, reviews } = this.state;
     return (
       <div className={styles.wrapper}>
         <div className={styles.container}>
-          <div className={styles.topRow}>
-            <div>
-              <span className={styles.price}>
-                ${price}&nbsp;
-              </span>
-              <span className={styles.diem}>
-                / night
-              </span>
-            </div>
-            <div>
-              <span className={styles.reviewWrapper}>
-                <span className={styles.star}>&#9733;</span>
-                <button className={styles.reviewButton} type="button">
-                  <span className={styles.rating}>{reviewScore} ({reviews})</span>
-                </button>
-              </span>
-            </div>
-          </div>
-          {/* <p id="price">{price}</p> */}
+          <TopRow data={this.state} />
           <Form data={this.state} handleSubmit={this.handleSubmit} />
           <Button />
           <div className={styles.nocharge}>
