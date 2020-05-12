3ad5d024bb0d8d8381266cc2fad0cec6c23453a9
Benjamin Barnett <ben.barnett@gmail.com>
2020-05-12 14:29:02 -0700

    Deconstruct a prop ESLint missed

--- client/Components/Form.jsx
+++ client/Components/Form.jsx
@@ -45,6 +45,7 @@ const ConnectedForm = (props) => {
   };
 
   const { duration, data, handleSubmit } = props;
+  const { maxGuests } = data;
   return (
     <div className={styles.formWrapper}>
       <form id="form" onSubmit={handleSubmit}>
@@ -56,7 +57,7 @@ const ConnectedForm = (props) => {
           Checkout
           <input type="date" onChange={onCheckoutChange} id="checkout" />
         </label>
-        <GuestDropdown duration={duration} maxGuests={data.maxGuests} />
+        <GuestDropdown duration={duration} maxGuests={maxGuests} />
       </form>
       <button className={styles.the_button} type="submit" onSubmit={handleSubmit} form="form">
         {duration ? 'Reserve' : 'Check availability'}
