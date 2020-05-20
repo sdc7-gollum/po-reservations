8bfd09abe2326ef9af868743d70e6654a2fe01c1
Max Ross <maxymus.ross@gmail.com>
2020-05-20 17:46:13 -0600

    add additional crud operations and amend readme to reflect additions

--- server/index.js
+++ server/index.js
@@ -20,6 +20,33 @@ app.route('/api/room/:id')
       }
       res.status(200).send(record);
     });
+  })
+  .post((req, res) => {
+    const room = new Room(req.body);
+    room.save((err) => {
+      if (err) {
+        // console.log('Post error:', err);
+        res.sendStatus(500);
+      }
+      res.status(201).end();
+    });
+  })
+  .put((req, res) => {
+    Room.updateOne({ id: req.params.id }, (err, record) => {
+      if (err) {
+        // console.log('Update error:', err);
+        res.sendStatus(500);
+      }
+      res.status(200).send(record);
+    });
+  })
+  .delete((req, res) => {
+    Room.deleteOne({ id: req.params.id }, (err) => {
+      if (err) {
+        // console.log('Delete error:', err);
+        res.sendStatus(500);
+      }
+      res.status(200).end();
+    });
   });
-
 app.listen(process.env.PORT, () => console.log(`Reservation listening at http://${process.env.HOST}:${process.env.PORT}`));
